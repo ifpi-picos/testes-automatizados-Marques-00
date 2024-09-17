1e2f71ddeb94cfd56f73e663c58b2e17d07b07ee
Marques-00 <amandafmarques00@gmail.com>
2024-09-17 10:50:55 -0300

    atividade

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,39 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    // o 'this' usa para chamar algo dentro do atributo privado 
+    //atributos
+  private numeroConta: number
+  private agencia: number
+  private saldo :number
+  private extrato: string []
+  
+  public constructor() {
+    this.numeroConta = 0
+    this.agencia = 0
+    this.saldo = 0
+    this.extrato = []
+  }
+  
+  //métodos
+    public depositar(valor: number) {
+      if (valor > 0) {
+        this.saldo += valor;
+        this.extrato.push(`Depósito de R$ ${valor.toFixed(2)}`);
+        return this.saldo
+      } else {
+        throw new Error('Valor inválido!') //criar novo erro
+      }
+    }
+  
+    public sacar (valor: number){
+      if(valor <= this.saldo && valor > 0) {
+        this.saldo -= valor;
+        return this.saldo
+      } else {
+        throw new Error('Valor inválido!') //criar novo erro
+      }
+    }
+  
+  
+  
+  }
+  
