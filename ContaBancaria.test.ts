6fedcb361b67e1f3b7ca7f983d67241b5ceb0648
Marques-00 <amandafmarques00@gmail.com>
2024-09-24 22:28:44 -0300

    atividadecompleta

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -3,49 +3,62 @@ import ContaBancaria from "./ContaBancaria.ts";
 
 describe("Testando classe ContaBancaria", () => {
   //criando duas contas
-  let conta: ContaBancaria ;
+  let conta: ContaBancaria;
   let segundaConta: ContaBancaria;
   beforeEach(() => { //antes de tudo tradução de beforeEach (antes de qualquer ccoisa, cria duas contas)
     conta = new ContaBancaria()
-    segundaConta= new ContaBancaria
+    segundaConta = new ContaBancaria
   })
 
-    test("método depositar", () =>{
-      expect(conta.depositar(200)).toBe(200)
-      //a expectativa é que seja depositado 200 e realmente fique 200
-})
+  test("método depositar", () => {
+    expect(conta.depositar(200)).toBe(200)
+    //a expectativa é que seja depositado 200 e realmente fique 200
+  })
 
-    test("método depositar teste de erro", () =>{
-      expect(() => conta.depositar(-100)).toThrow('Valor inválido!')
-      //expectativa que tente depositar -100 (mas foi foi feita a verificação dos números), então dará inválido
-    })
+  test("método depositar teste de erro", () => {
+    expect(() => conta.depositar(-100)).toThrow('Valor inválido!')
+    //expectativa que tente depositar -100 (mas foi foi feita a verificação dos números), então dará inválido
+  })
 
-    test("método sacar", () => {
-      conta.depositar(10)
-      expect(conta.sacar(5)).toBe(5);
-      //a expectativa é que eu saque 5 e fique com 5 na conta
-    })
-    test ("metodo sacar teste de erro", () => {
-      conta.depositar (20) //depositei 20
-      expect (()=>conta.sacar(30)).toThrow ('Valor inválido!')
-      //a expectativa é que tente sacar 30 (mas é mais do que tem na conta), então não dê certo
-   
-    })
-    test ("metodo transferir", ()=>{
-      conta.depositar (200) //depositei 200 na minha conta
-      expect (conta.transferir (100, segundaConta)).toBe (100)
-    //a expectativa é que eu tranfira 100 pra segunda conta e fique com 100
+  test("método sacar", () => {
+    conta.depositar(10)
+    expect(conta.sacar(5)).toBe(5);
+    //a expectativa é que eu saque 5 e fique com 5 na conta
+  })
 
-    })
-    test ("metodo transferir teste de erro" ,()=>{
-      conta.depositar (200)
-      expect (()=> conta.transferir (300, segundaConta)).toThrow("Valor inválido ou saldo de insuficiente")
-      //a expectativa é que tente tranferir 300 (mas é mais do que tem na conta), então não dá certo
-    })
+  test("metodo sacar teste de erro", () => {
+    conta.depositar(20) //depositei 20
+    expect(() => conta.sacar(30)).toThrow('Valor inválido!')
+    //a expectativa é que tente sacar 30 (mas é mais do que tem na conta), então não dê certo
+  })
+
+  test("metodo transferir", () => {
+    conta.depositar(200) //depositei 200 na minha conta
+    expect(conta.transferir(100, segundaConta)).toBe(100)
+    //a expectativa é que eu transfira 100 pra segunda conta e fique com 100
+  })
+
+  test("metodo transferir teste de erro", () => {
+    conta.depositar(200)
+    expect(() => conta.transferir(300, segundaConta)).toThrow("Valor inválido ou saldo de insuficiente")
+    //a expectativa é que tente transferir 300 (mas é mais do que tem na conta), então não dá certo
+  })
+
+  test("metodo consultar saldo", () => {
+    conta.depositar(200) //depositei 200
+    expect(conta.consultarSaldo()).toBe(200)
+    //a expectativa é que, ao consultar o saldo, realmente tenha 200
+  })
+
+  test("metodo exibir extrato", () => {
+    conta.depositar(200)
+    conta.sacar(100)
+    expect(conta.exibirExtrato()).toBe("1. Depósito de R$ 200.00\n2. Saque de R$ 100.00")
+  })
+
+  test ("metodo registrar operacao",() => {
+    expect(conta.registrar)
+  })
+
 });
 
-
-
-// let conta: ContaBancaria;
-// beforeEach(() => {
-// conta = new ContaBancaria();
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,55 +1,78 @@
 export default class ContaBancaria {
-    // o 'this' usa para chamar algo dentro do atributo privado 
-    //atributos
+  // o 'this' usa para chamar algo dentro do atributo privado 
+  //atributos
   private numeroConta: number
   private agencia: number
-  private saldo :number
-  private extrato: string []
-  
+  private saldo: number
+  private extrato: string[]
+
   public constructor() {
     this.numeroConta = 0
     this.agencia = 0
     this.saldo = 0
     this.extrato = []
   }
-  
+
   //métodos
-    public depositar(valor: number) {
-      if (valor > 0) {
-        this.saldo += valor;
-        this.extrato.push(`Depósito de R$ ${valor.toFixed(2)}`);
-        return this.saldo
-      } else {
-        throw new Error('Valor inválido!') //criar novo erro
-      }
-    }
-  
-    public sacar (valor: number){
-      if(valor <= this.saldo && valor > 0) {
-        this.saldo -= valor;
-        return this.saldo
-      } else {
-        throw new Error('Valor inválido!') //criar novo erro
-      }
-    }
-  
-    //públcio é quando você  faz, privado quando você não faz
-    private receberTransferencia (valor: number, conta: ContaBancaria){
-      conta.saldo+=valor
-      conta.extrato.push (`Transferencia de ${valor} recebida`)
-      
-    }
-    public transferir (valor: number, conta: ContaBancaria){
-      if (valor >0 && valor <= this.saldo){
-        conta.receberTransferencia (valor, conta)
-        this.saldo -= valor
-        this.extrato.push (`Transferencia de ${valor} recebida`)
-        return this.saldo
-      } else {
-        throw new Error ("Valor inválido ou saldo de insuficiente")
-      }
+  public depositar(valor: number) {
+    if (valor > 0) {
+      this.saldo += valor;
+      let descricao = `Depósito de R$ ${valor.toFixed(2)}`;
+      this.registrarOperacao(descricao)
+      return this.saldo
+    } else {
+      throw new Error('Valor inválido!') //criar novo erro
     }
   }
 
+  public sacar(valor: number) {
+    if (valor <= this.saldo && valor > 0) {
+      this.saldo -= valor;
+      let descricao = `Saque de R$ ${valor.toFixed(2)}`;
+      this.registrarOperacao(descricao)
+      return this.saldo
+    } else {
+      throw new Error('Valor inválido!') //criar novo erro
+    }
+  }
 
-  
+  //público é quando você faz, privado quando você não faz
+  private receberTransferencia(valor: number, conta: ContaBancaria) {
+    conta.saldo += valor
+    let descricao = `Transferencia de ${valor} recebida`
+    conta.registrarOperacao(descricao)}
+    
+    
+
+  public transferir(valor: number, conta: ContaBancaria) {
+    if (valor > 0 && valor <= this.saldo) {
+      conta.receberTransferencia(valor, conta)
+      this.saldo -= valor
+      let descricao=`Transferencia de ${valor} recebida`
+      this.registrarOperacao(descricao)
+      return this.saldo
+    } else {
+      throw new Error("Valor inválido ou saldo de insuficiente")
+    }
+  }
+
+  public consultarSaldo() {
+    return this.saldo
+  }
+
+  public exibirExtrato() {
+    let extratoString = ""
+    this.extrato.forEach((operacao, index) => {
+      extratoString += `${index + 1}. ${operacao}\n`
+    })
+    return extratoString.trim() //trim remove caracteres especial e espaços vazios
+  }
+
+  private registrarOperacao (descricao:string){
+    this.extrato.push (descricao)
+  }
+
+
+}
+
+
